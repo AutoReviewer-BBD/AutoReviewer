af3dc9b7491cd58d3e302740bec178633427b4a4
Thapelo Thoka <Thapelo.Thoka@bbd.co.za>
2024-04-09 19:44:15 +0200

    CLI outline

---
+++ AutoCli/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using AutoCli.Commands;
+using System.Text.Json;
+
+namespace AutoCli {
+    class Program {
+        static async Task Main(string[] args) {
+
+            Console.WriteLine("Autoreview intitiated! Enter a command.");
+
+            while (true) {
+                string input = Console.ReadLine();
+                string command = inputs[0].ToLower();
+                string[] inputs = input.Split(' ');
+
+                else if (command == "help") {
+                    HelpCommand.Run();
+                }
+                else if (command == "clear") {
+                    ClearCommand.Run();
+                }
+                else if (command == "login") {
+                    //To be implemented
+                }
+                else if (command == "history") {
+                    if (accessToken != null) {
+                        HistoryCommand.Run();
+                    }
+                    else {
+                        Console.WriteLine("Unauthorized User. Login to view history");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid command. Try 'help'.");
+                }
+            }
+        }
+    }
+}
