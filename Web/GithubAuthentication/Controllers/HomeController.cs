c145d804e460219ed8ffe19d741f332968469d46
ThapeloThoka <156646690+ThapeloThoka@users.noreply.github.com>
2024-04-11 16:16:07 +0200

    Basic Web UI

--- Web/GithubAuthentication/Controllers/HomeController.cs
+++ Web/GithubAuthentication/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -6,18 +7,22 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using Octokit;
 namespace GithubAuthentication.Controllers
+
 {
     public class HomeController : Controller
     {
-        public async Task<ActionResult> Index()
+        public static string AccessToken { get; set; }
+
+        public Task<ActionResult> Index()
         {
             ViewBag.ClientId = ConfigurationManager.AppSettings["GithubClientId"].ToString();
             ViewBag.RedirectUrl = ConfigurationManager.AppSettings["RedirectUri"].ToString();
-            return View();
-        }
 
+            return Task.FromResult<ActionResult>(View());
+        }
         public async Task<ActionResult> GithubLogin(string code)
         {
             var client = new HttpClient();
@@ -34,18 +39,68 @@ namespace GithubAuthentication.Controllers
             var values = HttpUtility.ParseQueryString(responseContent);
             var access_token = values["access_token"];
             var client1 = new GitHubClient(new ProductHeaderValue("reviewerauto126"));
-            var tokenAuth = new Credentials(access_token);
+            if(access_token!= null) 
+                AccessToken = access_token;
+
+            var tokenAuth = new Credentials(AccessToken);
+
             client1.Credentials = tokenAuth;
             var user = await client1.User.Current();
             var email = user.Email;
-            return View(user);
+            var repos = await client1.Repository.GetAllForCurrent();
+
+            var repositories = await client1.Repository.GetAllForCurrent();
+            ViewBag.RepositoriesJson = JsonConvert.SerializeObject(repositories);
+
+            // Create a list to hold repositories with branches
+            var repositoriesWithBranches = new List<RepositoryWithBranches>();
+
+            // Fetch branches for each repository
+            foreach (var repo in repositories)
+            {
+                var branches = await client1.Repository.Branch.GetAll(repo.Owner.Login,repo.Name);
+                var repoWithBranches = new RepositoryWithBranches
+                {
+                    Repository = repo,
+                    Branches = branches.Select(b => b.Name).ToList()
+                };
+                repositoriesWithBranches.Add(repoWithBranches);
+            }
+            
+
+            return View(repositoriesWithBranches);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        public async Task<ActionResult> Repositories()
+{
+    // Get the access token from session or wherever you store it
+    var accessToken = ""; // Retrieve the access token from the session or wherever you store it
+
+    if (string.IsNullOrEmpty(accessToken))
+    {
+        // Redirect to login page if access token is not available
+        return RedirectToAction("Index");
+    }
+
+    try
+    {
+        var client = new GitHubClient(new ProductHeaderValue("reviewerauto126"));
+        var tokenAuth = new Credentials(accessToken);
+        client.Credentials = tokenAuth;
+
+        // Get the current user's repositories
+        var repositories = await client.Repository.GetAllForCurrent();
+
+        // Pass the repositories to the view
+        return View(repositories);
+    }
+    catch (Exception ex)
+    {
+        // Handle any errors, maybe redirect to an error page
+        return View("Error");
+    }
+}
+
     }
 }
---
+++ Web/GithubAuthentication/Controllers/RepositoryWithBranches.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Octokit;
+
+namespace GithubAuthentication.Controllers
+{
+    public class RepositoryWithBranches : Controller
+    {
+    public Repository Repository { get; set; }
+        public List<string> Branches { get; set; }
+    }
+}
