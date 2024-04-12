ce9d01282ca1c102673583b0d6864584f1491869
Kat <Katlego.Kungoane@bbd.co.za>
2024-04-12 09:25:36 +0200

    fixed program.cs

--- ReviewAPI/Program.cs
+++ ReviewAPI/Program.cs
@@ -8,7 +8,7 @@ using Api.Interfaces;
 using Api.Repositories;
 using RealConnection.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -63,7 +63,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
      };
  });
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
